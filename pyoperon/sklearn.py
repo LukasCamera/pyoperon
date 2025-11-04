9151b604697d041bf76aa49d81156e151ac56b43
Lukas Kammerer <lukas.kammerer@fh-hagenberg.at>
2025-11-04 15:35:19 +0100

    Fix check for matching primitive set.

--- pyoperon/sklearn.py
+++ pyoperon/sklearn.py
@@ -582,7 +582,8 @@ class SymbolicRegressor(BaseEstimator, RegressorMixin):
 
                 try:
                     for i in range(tree.Length):
-                        pset.IsEnabled(tree.Nodes[i].HashValue)
+                        if tree.Nodes[i].Type != op.NodeType.Variable:
+                            pset.IsEnabled(tree.Nodes[i].HashValue)
                 except RuntimeError:
                     raise ValueError(f'Symbol {tree.Nodes[i].Type} found in initial_population is not in the allowed_symbols set.')
                 
